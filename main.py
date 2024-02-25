1ac7307d458fbd3ce70e6bc40722d12746a7820c
Sergio Andrés Herrera Velásquez <sergioandresherrera@gmail.com>
2024-02-24 22:11:13 -0500

    added main.py as the entry point for the scraper

---
+++ main.py
@@ -0,0 +1,13 @@
+
+
+"""Provide the entry point for the scraper.
+
+This module allows the user access the entry point of the scraper.
+
+The module contains the following functions:
+
+- `add(a, b)` - Returns the sum of two numbers.
+- `subtract(a, b)` - Returns the difference of two numbers.
+- `multiply(a, b)` - Returns the product of two numbers.
+- `divide(a, b)` - Returns the quotient of two numbers.
+"""
